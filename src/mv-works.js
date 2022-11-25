7c47f539db37a8f1531548d513b244535afcbfb9
KodaiC <4620069@ed.tus.ac.jp>
2022-11-25 20:23:36 +0900

    2022年理大祭ページの修正、MV部作品集の微調整

--- src/mv-works.js
+++ src/mv-works.js
@@ -1,23 +1,42 @@
+(function () {
+    Vue.component("mv-work", {
+        props: [
+            "item"
+        ],
+        template: `
+        <div class="card col-11 col-lg-5">
+            <div class="card-body">
+                <h4 class="title">「{{item.title}}」</h4>
+                <p class="description" v-if="item.description">{{item.description}}</p>
+                <div class="authors row m-3">
+                    <p>作者： </p>
+                    <div>
+                        <p v-for="author in item.authors">{{author}}</p>
+                    </div>
+                </div>
+                <div class="subauthors row m-3" v-if="item.subauthors">
+                    <p>協力者： </p>
+                    <div>
+                        <p v-for="subauthor in item.subauthors">{{subauthor}}</p>
+                    </div>
+                </div>
+                <div class="embed embed-responsive embed-responsive-16by9" v-html="item.embed"></div>
+            </div>
+        </div>
+   `});
 
-(function(){
     var vm = new Vue({
-        el: '#movie-works',
+        el: '#mv-works',
         data: {
-            items:null,
-        },
-        //thisはdataを指します
-        methods:{
-            
+            items: null
         },
         mounted: function () {
-            var self=this;
+            var self = this;
             axios.get("data/mv-works.json").then(response => {
                 self.items = response.data;
-                console.log(typeof(self.items));
-                console.log(self.items);
             }).catch(error => {
                 console.log(error);
             });
         }
-        });
+    });
 })();
